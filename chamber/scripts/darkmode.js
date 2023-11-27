213d6904648b89dd20841c0d0247b3410b1a8599
Gloria-Ekpo <104460473+Gloria-Ekpo@users.noreply.github.com>
2023-11-27 08:44:31 -0700

    menu

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,19 +1,23 @@
 const darkmode = document.querySelector('#dark-mode');
 const bodyelt = document.querySelector("body");
 
-darkmode.addEventListener('click',()=>{
-    if (darkmode.textContent == 'DARK' ){
-        document.documentElement.style.setProperty('--text-color', 'rgb(249, 245, 245)');        
-        document.documentElement.style.setProperty('--background-color', 'black');    
-            
+darkmode.addEventListener('click', () => {
+    if (darkmode.textContent == 'DARK') {
+        document.documentElement.style.setProperty('--text-color', 'white');
+        document.documentElement.style.setProperty('--background-color', 'grey');
+        document.documentElement.style.setProperty("--hover-background-color", "#dda15e");
         bodyelt.style.backgroundColor = 'black';
-        darkmode.textContent = 'LIGHT'
-    }
-    else{
-        document.documentElement.style.setProperty('--text-color', 'black');        
-        document.documentElement.style.setProperty('--background-color', 'rgb(249, 245, 245)');        
-        
+        darkmode.textContent = 'LIGHT';
+          header.style.backgroundColor = "var(--header-background-color)";
+          header.querySelector("h1").style.color = "white";
+          nav.style.backgroundColor = "#2c2c2c";
+
+
+
+    } else {
+        document.documentElement.style.setProperty('--text-color', 'black');
+        document.documentElement.style.setProperty('--background-color', 'rgb(249, 245, 245)');
         bodyelt.style.backgroundColor = 'white';
-        darkmode.textContent = 'DARK'
+        darkmode.textContent = 'DARK';
     }
-})
+});

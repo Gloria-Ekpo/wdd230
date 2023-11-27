176b87d052ea6709f3a516c2bc74094ac8908b3d
Gloria-Ekpo <104460473+Gloria-Ekpo@users.noreply.github.com>
2023-11-27 00:28:41 -0700

    changes

--- chamber/scripts/hamburger-menu.js
+++ chamber/scripts/hamburger-menu.js
@@ -1,23 +1,15 @@
-// const hidebutton = document.querySelector(".menu-hide");
-// const showbutton = document.querySelector(".menu-show");
-// const navmenu = document.querySelector("#nav-menu"); 
+const hidebutton = document.querySelector(".menu-hide");
+const showbutton = document.querySelector(".menu-show");
+const navmenu = document.querySelector("#nav-menu");
 
-// hidebutton.addEventListener('click',() => {
-//     hidebutton.classList.toggle('showing');
-//     showbutton.classList.toggle('showing');
-//     navmenu.classList.toggle('showing');
-// });
+hidebutton.addEventListener('click', () => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');
+});
 
-// showbutton.addEventListener('click',() => {
-//     hidebutton.classList.toggle('showing');
-//     showbutton.classList.toggle('showing');
-//     navmenu.classList.toggle('showing');        
-// });
-
-const navbutton = document.querySelector("#nav-button");
-const navmenu = document.querySelector("#nav-menu"); 
-
-navbutton.addEventListener('click',() => {
-    navbutton.classList.toggle('showing');    
+showbutton.addEventListener('click', () => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
     navmenu.classList.toggle('showing');
 });

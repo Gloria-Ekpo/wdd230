9020dd2e9f9ce39dd8a7353b1ccc52bc83bef0f8
Gloria-Ekpo <104460473+Gloria-Ekpo@users.noreply.github.com>
2023-11-27 11:49:32 -0700

    added

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,32 @@
-// Get temperature and wind speed values from HTML
-let tempF = parseFloat(document.getElementById('temperature').textContent);
-let speed = parseFloat(document.getElementById('windspeed').textContent);
+document.addEventListener('DOMContentLoaded', function () {
+    // Get windspeed and temperature from the page
+    // Call the windchill function
+    const temperatureSpan = document.getElementById("temperature");
+    const windspeedSpan = document.getElementById("windspeed");
+    const windchillSpan = document.getElementById("windchill");
 
-// Check if input values meet the specification limits
-if (tempF <= 50 && speed > 3.0) {
-    let windChill = calculateWindChill(tempF, speed);
-    document.getElementById('windchill').textContent = windChill.toFixed(2);
-} else {
-    document.getElementById('windchill').textContent = "N/A";
-}
+    // Check if all necessary elements are available
+    if (temperatureSpan && windspeedSpan && windchillSpan) {
+        const temperature = parseInt(temperatureSpan.textContent);
+        const windspeed = parseInt(windspeedSpan.textContent);
 
-// Function to calculate wind chill
-function calculateWindChill(tempF, speed) {
-    return 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+        showWindChill(temperature, windspeed, windchillSpan);
+    }
+});
+
+function showWindChill(temp, speed, windchillSpan) {
+    // Set default windchill message
+    let message = "N/A";
+
+    // Check for valid temp/windspeed    
+    if (temp <= 50 && speed > 3) {
+        // Get chillfactor and windchill calculation
+        let chillfactor = Math.pow(speed, 0.16);
+        let chill = Math.round(35.74 + (0.6215 * temp) - (35.75 * chillfactor) + (0.4275 * temp * chillfactor));
+
+        // Create windchill message
+        message = `${chill}`;
+    }
+    // Set the DOM element windchillmessage
+    windchillSpan.textContent = message;
 }

3c6dae1b8bb682d91cba3ff70b7ace54a1abb8be
Gloria-Ekpo <104460473+Gloria-Ekpo@users.noreply.github.com>
2023-12-11 22:45:04 -0700

    home

--- bountiful/scripts/scripts.js
+++ bountiful/scripts/scripts.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    lastModified('lastModified');
-});
-
-function lastModified(id) {
-    const lastModifiedDate = document.lastModified;
-    const formattedDate = new Date(lastModifiedDate).toLocaleString();
-    document.getElementById(id).textContent = formattedDate;
-}
+document.getElementById('dateModified').textContent = document.lastModified;
